b54845c1f692eb4ada4982804717ca2976b11881
Koichi Sasada <ko1@atdot.net>
2015-02-06 01:58:44 +0900

    support file contents

--- lib/pretty_backtrace.rb
+++ lib/pretty_backtrace.rb
@@ -6,6 +6,8 @@ module PrettyBacktrace
     truncate_length: 20,
     disabled_exception_classes: {},
     effective_lines: 0, # 0 is infinite
+    file_contents: true,
+    file_contents_lines: 2,
 
     multi_line: false,
     multi_line_truncate_length: 60,
@@ -43,7 +45,7 @@ module PrettyBacktrace
             lvs_val = {}
           end
 
-          modify_trace_line loc, lvs_val
+          modify_trace_line loc, loc.absolute_path, loc.lineno, lvs_val
         }.compact
         e.set_backtrace pretty_backtrace
       }
@@ -53,6 +55,63 @@ module PrettyBacktrace
       fail "PrettyBacktrace BUG"
     end
   }
+
+  def self.iseq_local_variables iseq
+    _,_,_,_,arg_info,name,path,a_path,_,type,lvs, * = iseq.to_a
+    lvs
+  end
+
+  #
+  # local_variables_values is a Hash object containing pairs of
+  # a local variable name and value of local variable.
+  #
+  def self.modify_trace_line backtrace_location, absolute_path, lineno, local_variables_values
+    trace_line = backtrace_location.to_s
+
+    unless local_variables_values.empty?
+      if CONFIG[:multi_line]
+        indent = ' ' * CONFIG[:multi_line_indent]
+        additional = ''
+
+        # file scope
+        if CONFIG[:file_contents] && File.exists?(absolute_path)
+          fclines = CONFIG[:file_contents_lines]
+          start_line = lineno - 1 - 1 * fclines
+          start_line = 0 if start_line < 0
+
+          additional << indent + "[FILE]\n"
+          additional << open(absolute_path){|f| f.readlines[start_line, 1 + 2 * fclines]}.map.with_index{|line, i|
+            ln = start_line + i + 1
+            line = line.chomp
+            '%s%4d|%s' % [ln == lineno ? indent[0..-3] + "->" : indent, ln, line]
+          }.join("\n")
+          additional << "\n"
+          additional << "\n"
+        end
+
+        # local variables
+        unless local_variables_values.empty?
+          additional << indent + "[LOCAL VARIABLES]\n"
+          additional << local_variables_values.map{|lv, v|
+            v = v[0..CONFIG[:multi_line_truncate_length]] + '...' if v.length > CONFIG[:multi_line_truncate_length]
+            indent + "  #{lv} = #{v.to_s}"
+          }.join("\n") + "\n"
+        end
+
+        trace_line = "#{trace_line}\n#{additional}" unless additional.empty?
+      else
+        additional = local_variables_values.map{|lv, v|
+          v = v[0..CONFIG[:truncate_length]] + '...' if v.length > CONFIG[:truncate_length]
+          "#{lv} = #{v.to_s}"
+        }.join(", ")
+        trace_line = "#{trace_line} (#{additional})"
+      end
+    end
+
+    trace_line
+  end
+
+  # configuration
 
   def self.enable
     if block_given?
@@ -74,48 +133,12 @@ module PrettyBacktrace
     end
   end
 
-  def self.iseq_local_variables iseq
-    _,_,_,_,arg_info,name,path,a_path,_,type,lvs, * = iseq.to_a
-    lvs
-  end
-
-  #
-  # local_variables_values is a Hash object containing pairs of
-  # a local variable name and value of local variable.
-  #
-  def self.modify_trace_line backtrace_location, local_variables_values
-    trace_line = backtrace_location.to_s
-
-    unless local_variables_values.empty?
-      if CONFIG[:multi_line]
-        indent = ' ' * CONFIG[:multi_line_indent]
-        additional = ''
-
-        # local variables
-        unless local_variables_values.empty?
-          additional << indent + "local variables:\n"
-          additional << local_variables_values.map{|lv, v|
-            v = v[0..CONFIG[:multi_line_truncate_length]] + '...' if v.length > CONFIG[:multi_line_truncate_length]
-            indent + "  #{lv} = #{v.to_s}"
-          }.join("\n") + "\n"
-        end
-
-        trace_line = "#{trace_line}\n#{additional}" unless additional.empty?
-      else
-        additional = local_variables_values.map{|lv, v|
-          v = v[0..CONFIG[:truncate_length]] + '...' if v.length > CONFIG[:truncate_length]
-          "#{lv} = #{v.to_s}"
-        }.join(", ")
-        trace_line = "#{trace_line} (#{additional})"
-      end
-    end
-
-    trace_line
-  end
-
-
   def self.multi_line=(setting)
-    CONFIG[:multi_line] = true
+    CONFIG[:multi_line] = setting
+  end
+
+  def self.file_contents=(setting)
+    CONFIG[:file_contents] = setting
   end
 
   def self.effective_lines=(lines)
